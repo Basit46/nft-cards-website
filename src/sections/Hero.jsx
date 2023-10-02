2181b18c05f20ea33c7d824535c42ecc37c66dcc
Basit46 <Hassanbasit1990@gmail.com>
2023-10-02 12:52:12 +0100

    Updated Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
       <h1 className="w-full px-[30px] text-left font-Anton text-[#f54a14] text-[4.8rem] vsm:text-[6rem] xmd:text-[8.72rem] tracking-[-2.76px] leading-[1] xmd:leading-[136.62px]">
         DIGITAL COLLECTION CARDS
       </h1>
-      <div className="relative mx-[30px] h-[700px] mt-[20px] rounded-[10px] overflow-hidden">
+      <div className="relative mx-[30px] h-[350px] vsm:h-[400px] md:h-[700px] mt-[20px] rounded-[10px] overflow-hidden">
         {images.map((img, index) => (
           <img
             key={index}
@@ -45,29 +45,29 @@ const Hero = () => {
       <div className="mt-[40px] w-full overflow-x-hidden border-y-[1px] border-black py-[30px]">
         <div className="slide w-fit flex gap-[30px]">
           <div className="w-fit pl-[30px] flex flex-nowrap items-center gap-[30px]">
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
           </div>
           <div className="w-fit flex flex-nowrap items-center gap-[30px]">
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
-            <h1 className="font-Anton text-[1.875rem] whitespace-nowrap">
+            <h1 className="font-Anton text-[1.2rem] sm:text-[1.875rem] whitespace-nowrap">
               Explore This Collection Carefully
             </h1>
             <div className="h-[10px] w-[10px] bg-[#f54a14]"></div>
